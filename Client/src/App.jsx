0c29abcb58cebcd45e45a5b77f9fcc8cc1333083
ombansal1234 <abc426037@gmail.com>
2024-11-29 20:56:21 +0530

    commit

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Navbar from './components/Navbar'
 import { Routes, Route } from 'react-router-dom'
 import HomePage from "./pages/HomePage";
@@ -6,10 +6,25 @@ import SignUpPage from "./pages/SignUpPage";
 import LoginPage from "./pages/LoginPage";
 import SettingsPage from "./pages/SettingsPage";
 import ProfilePage from "./pages/ProfilePage";
+import {Loader} from "lucide-react";
 
 
 
 const App = () => {
+  const {authUser,checkAuth} = useAuthStore()
+
+  useEffect(() =>{
+    checkAuth();
+  },[checkAuth]);
+
+  console.log({authUser});
+
+  if(true) return(
+    <div className="flex items-center justify-center h-screen">
+      <Loader className="size-10 animate-spin"/>
+    </div>
+  )
+
   return (
     <div>
 
