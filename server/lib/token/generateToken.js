61685443d36126b309e18a127d2d6332454898a6
RishabSharma2003 <141629276+RishabSharma2003@users.noreply.github.com>
2024-12-15 04:24:09 +0530

    commit

--- server/lib/token/generateToken.js
+++ server/lib/token/generateToken.js
@@ -4,5 +4,5 @@ export const generateToken=(userId,res)=>{
     const token=jwt.sign({userId},process.env.JWT_SECRET,{expiresIn:'7d'})
     console.log(token)
 
-    res.cookie('jwt',token,{maxAge:7*24*60*60*1000,httpOnly: true,secure: true,sameSite: 'None'})
+    res.cookie('jwt',token,{maxAge:7*24*60*60*1000})
 }
